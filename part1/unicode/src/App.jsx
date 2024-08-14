5ccc9de20853b502c4fb0b109b7594a6c25827cd
Ahmadsucks <86570807+Ahmadsucks@users.noreply.github.com>
2024-08-15 01:11:22 +0200

    1.11

--- part1/unicode/src/App.jsx
+++ part1/unicode/src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react'
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const StatisticLine = ({ text, value }) => {
-  if (text === 'positive') return <p>{text} {value} %</p>
-  return <p>{text} {value}</p>
+if (text === 'positive') {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value} %</td>
+    </tr>
+  )
+}
+return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+  
 }
 
 const Statistics = ({ good, neutral, bad, all, average, positive}) => {
@@ -11,12 +25,16 @@ const Statistics = ({ good, neutral, bad, all, average, positive}) => {
   return (
     <>
       <h1>statistics</h1>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={average} />
-      <StatisticLine text='positive' value={positive} />
+      <table>
+        <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={all} />
+        <StatisticLine text='average' value={average} />
+        <StatisticLine text='positive' value={positive} />
+        </tbody>  
+      </table>
     </>
  )
 }
